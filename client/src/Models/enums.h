c9a2bb61746ba89bd60b1e1c223e33101edf6ddd
Parsa Saberi <parsa.xmule@gmail.com>
2021-02-08 06:53:18 +0330

    create enums.h
    ant team
    ant type
    cell type
    resource type

---
+++ client/src/Models/enums.h
@@ -0,0 +1,26 @@
+#ifndef AIC21_CLIENT_CPP_ENUMS_H
+#define AIC21_CLIENT_CPP_ENUMS_H
+
+enum AntTeam {
+    ALLY,
+    ENEMY
+};
+
+enum AntType {
+    SARBAAZ,
+    KARGAR
+}
+
+enum CellType {
+    BASE,
+    EMPTY,
+    WALL
+}
+
+enum ResourceType {
+    BREAD,
+    GRASS,
+    NONE
+}
+
+#endif //AIC21_CLIENT_CPP_ENUMS_H
