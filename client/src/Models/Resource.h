5d6f8add58727aa9a0afe742d37184cf720b15b1
Parsa Saberi <parsa.xmule@gmail.com>
2021-02-11 06:06:30 +0330

    add resource

---
+++ client/src/Models/Resource.h
@@ -0,0 +1,22 @@
+#ifndef AIC21_CLIENT_CPP_RESOURCE_H
+#define AIC21_CLIENT_CPP_RESOURCE_H
+
+#include "enums.h"
+
+class Resource {
+
+    public:
+
+    Resource(ResourceType type, int amount);
+    ResourceType getType();
+    int getAmount();
+
+
+    private:
+
+    ResourceType type_;
+    int amount_;
+
+};
+
+#endif //AIC21_CLIENT_CPP_RESOURCE_H
