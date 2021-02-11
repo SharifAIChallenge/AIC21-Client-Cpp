25201b611549f1bd767d07bac037ff7831be620f
Parsa Saberi <parsa.xmule@gmail.com>
2021-02-11 06:07:05 +0330

    add chatbox

---
+++ client/src/Models/ChatBox.h
@@ -0,0 +1,25 @@
+#ifndef AIC21_CLIENT_CPP_CHATBOX_H
+#define AIC21_CLIENT_CPP_CHATBOX_H
+
+using namespace std;
+#include <vector>
+#include "enums.h"
+#include "Chat.h"
+
+
+class ChatBox {
+
+    private:
+
+    vector<Chat> all_chats_;
+
+
+    public:
+
+    ChatBox(vector<Chat> allChats);
+    vector<Chat> getAllChatsOfTurn(int turnNumber);
+    vector<Chat> getAllChats();
+
+}
+
+#endif //AIC21_CLIENT_CPP_CHATBOX_H
