fc8cfda6a1fd99eaddb38bd05bac08daafd8d8ce
Parsa Saberi <parsa.xmule@gmail.com>
2021-02-11 06:50:42 +0330

    fix cell.h

--- client/src/Models/Cell.h
+++ client/src/Models/Cell.h
@@ -32,35 +32,4 @@ class Cell {
 };
 
 
-
-// class Cell {
-// public:
-//     Cell(int row, int col);
-
-//     Cell(const Cell&);
-
-
-//     ~Cell() = default;
-
-//     bool operator==(const Cell &rhs) const;
-
-//     bool operator!=(const Cell &rhs) const;
-
-//     int getRow() const;
-
-//     int getCol() const;
-
-//     std::vector<const Unit *> getUnits() const;
-
-// private:
-//     int row_;
-//     int col_;
-//     std::vector<const Unit *> units_;
-
-//     friend class TurnMessage;
-//     friend class ShutdownMessage;
-//     friend class Map;
-// };
-
-
 #endif //AIC21_CLIENT_CPP_CELL_H
